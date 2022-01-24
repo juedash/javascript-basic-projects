3db1b59ba0bcad86805e45a35f59cf4c3045175a
Jueda Sherifi <jueda.sherifi@comm-it.it>
2022-01-24 17:59:04 +0100

    DOM manipulation: Guess my number - DRY

--- 01-Guess-My-Number/script.js
+++ 01-Guess-My-Number/script.js
@@ -13,6 +13,8 @@ const generateSecretNumber = () => {
   return Math.trunc(Math.random() * 20) + 1;
 };
 
+let bodyBackGround = document.querySelector('body').style.backgroundColor;
+let numberWidth = document.querySelector('.number').style.width;
 let secretNumber = generateSecretNumber();
 let score = 20;
 let highScore = 0;
@@ -27,9 +29,8 @@ document.querySelector('.check').addEventListener('click', () => {
   } else if (guess === secretNumber) {
     displayMessage('🎉 Correct Number!');
     displayNumber(secretNumber);
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-
+    bodyBackGround = '#60b347';
+    numberWidth = '30rem';
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
@@ -54,6 +55,6 @@ document.querySelector('.again').addEventListener('click', () => {
   displayScore(score);
   displayNumber('?');
   document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  bodyBackGround = '#222';
+  numberWidth = '15rem';
 });
